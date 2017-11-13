a58bceaa610fd9d3b480c2961b2897e4df9fc5e3
Alice <794926872@qq.com>
2017-11-13 17:34:07 +0800

    10.7嵌套内部类练习题代码提交。

---
+++ src/main/java/com/hjp/javaSource/ThinkingInJava/InnerClasses/E19_InnerInsideInner.java
@@ -0,0 +1,43 @@
+package com.hjp.javaSource.ThinkingInJava.InnerClasses;
+
+/**
+ * @author huangjp 2017-11-13 17:01
+ * 内部类嵌套内部类，注意编译器生成的.class文件的名字
+ * E19_InnerInsideInner：    E19_InnerInsideInner.class
+ * InnerClass1：             E19_InnerInsideInner$InnerClass1.class
+ * InnerClass2：             E19_InnerInsideInner$InnerClass1$InnerClass2.class
+ **/
+public class E19_InnerInsideInner {
+
+    static class InnerClass1{
+        class InnerClass2{
+            void f(){
+                System.out.println("InnerClass2.f()...");
+            }
+        }
+
+        InnerClass2 get2(){
+            return new InnerClass2();
+        }
+    }
+
+    public static void main(String[] args) {
+        InnerClass1 i1 = new InnerClass1();
+        InnerClass1.InnerClass2 i2 = i1.get2();
+        i2.f();
+    }
+    /*
+        Output : InnerClass2.f()...
+     */
+}
+
+class OtherClass{
+    public static void main(String[] args) {
+        E19_InnerInsideInner.InnerClass1 i1 = new E19_InnerInsideInner.InnerClass1();
+        E19_InnerInsideInner.InnerClass1.InnerClass2 i2 = i1.get2();
+        i2.f();
+    }
+    /*
+        Output : InnerClass2.f()...
+     */
+}
---
+++ src/main/java/com/hjp/javaSource/ThinkingInJava/InnerClasses/E20_InterfaceWithNested.java
@@ -0,0 +1,33 @@
+package com.hjp.javaSource.ThinkingInJava.InnerClasses;
+
+/**
+ * @author huangjp 2017-11-13 17:20
+ * 接口内部的类
+ **/
+public class E20_InterfaceWithNested {
+    public static void main(String[] args) {
+        WithNested.Nested nested = new B2.Nested(1111);
+        nested.f();
+    }
+}
+/*
+    Output : Nested.f()...
+ */
+
+interface WithNested{
+    class Nested{
+        int i;
+
+        public Nested(int i) {
+            this.i = i;
+        }
+
+        void f(){
+            System.out.println("Nested.f()...");
+        }
+    }
+}
+
+class B2 implements WithNested{
+
+}
