e1e2a351dbc4f22d376d984997b5e78ac26d1d0f
Java兔 <794926872@qq.com>
2019-09-23 16:06:11 +0800

    访问权限修饰符案例提交。

---
+++ src/main/java/com/hjp/javaSource/anotherPackage/ProtectedChildClass.java
@@ -0,0 +1,13 @@
+package com.hjp.javaSource.anotherPackage;
+
+import com.hjp.javaSource.test.ProtectedClass;
+
+/**
+ * @author huangjp 2019/9/23 4:04 PM
+ **/
+public class ProtectedChildClass extends ProtectedClass {
+
+    public void f(){
+        protectedMethod();  //不同包子类可访问父类的protected方法
+    }
+}
---
+++ src/main/java/com/hjp/javaSource/test/ProtectedClass.java
@@ -0,0 +1,11 @@
+package com.hjp.javaSource.test;
+
+/**
+ * @author huangjp 2019/9/23 4:03 PM
+ **/
+public class ProtectedClass {
+
+    protected void protectedMethod(){
+        System.out.println("I'm protected method");
+    }
+}
