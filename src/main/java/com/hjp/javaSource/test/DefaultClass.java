da5c753e19bad6e41e556e0a62127270abde96ee
Java兔 <794926872@qq.com>
2019-09-23 16:09:25 +0800

    访问权限修饰符案例提交。

---
+++ src/main/java/com/hjp/javaSource/test/DefaultClass.java
@@ -0,0 +1,12 @@
+package com.hjp.javaSource.test;
+
+/**
+ * @author huangjp 2019/9/23 4:08 PM
+ **/
+public class DefaultClass {
+
+    void defaultMethod(){
+        System.out.println("I'm default method，仅支持同类或同包访问");
+    }
+
+}
