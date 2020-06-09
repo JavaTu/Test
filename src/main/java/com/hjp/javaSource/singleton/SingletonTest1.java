505f4b5e72bee785621360542f276c7b52ba27ac
Java兔 <794926872@qq.com>
2020-06-09 13:36:05 +0800

    单例模式增加内部类及枚举两种写法。

---
+++ src/main/java/com/hjp/javaSource/singleton/SingletonTest1.java
@@ -0,0 +1,20 @@
+package com.hjp.javaSource.singleton;
+
+/**
+ * @ClassName: SingletonTest1
+ * @Description: 内部类单例模式
+ * @Author: huangjp
+ * @Date: 2020/6/9 11:57
+ */
+public class SingletonTest1 {
+
+    private SingletonTest1() {}
+
+    public static SingletonTest1 getInstance() {
+        return Mgr01Holder.INSTANCE;
+    }
+
+    private static class Mgr01Holder {
+        private static final SingletonTest1 INSTANCE = new SingletonTest1();
+    }
+}
---
+++ src/main/java/com/hjp/javaSource/singleton/SingletonTest2.java
@@ -0,0 +1,17 @@
+package com.hjp.javaSource.singleton;
+
+/**
+ * @ClassName: SingleTest2
+ * @Description: 枚举单例模式
+ * @Author: huangjp
+ * @Date: 2020/6/9 11:57
+ */
+public enum SingletonTest2 {
+
+    INSTANCE;
+
+    public static SingletonTest2 getInstance(){
+
+        return INSTANCE;
+    }
+}
