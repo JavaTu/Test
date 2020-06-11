7b4e7e4e14c1829e2b4d701cb0dd2aa1ee325587
Java兔 <794926872@qq.com>
2020-06-11 16:38:05 +0800

    位运算测试类。

--- src/main/java/com/hjp/javaSource/javaBase/BitTest.java
+++ src/main/java/com/hjp/javaSource/javaBase/BitTest.java
@@ -9,8 +9,31 @@ package com.hjp.javaSource.javaBase;
 public class BitTest {
 
     public static void main(String[] args) {
-        int i1 = 0;
-        System.out.println(i1 >> 1);
+        int a = 1;  // 0001
+        int b = 3;  // 0011
+        int c = 9;  // 1001
+        int d = -2; // 1111 1111 1111 1111 1111 1111 1111 1111，32位
+
+        // 与：&
+        System.out.println("0001 & 0011：" + Integer.toBinaryString(a & b));     // 0001，都是1才是1，否则都是0
+
+        // 或：|
+        System.out.println("0001 | 0011：" + Integer.toBinaryString(a | b));     // 0011，有一个1就是1
+
+        // 异或：^
+        System.out.println("0001 ^ 0011：" + Integer.toBinaryString(a ^ b));     // 0010，相同为0，不同为1
+
+        // 左移：<<
+        System.out.println("1001 << 2：" + Integer.toBinaryString(c << 2));      // 100100，右边加2个0
+
+        // 右移：>>
+        System.out.println("1001 >> 2：" + Integer.toBinaryString(c >> 2));      // 10，右边砍掉2位
+
+        // 无符号右移：>>>
+        System.out.println("1001 >>> 2：" + Integer.toBinaryString(c >>> 2));    // 10
+
+        System.out.println("-2的二进制：" + Integer.toBinaryString(d));
+        System.out.println("-1 >>> 2 ：" + Integer.toBinaryString(d >>> 2));
     }
 
 }
