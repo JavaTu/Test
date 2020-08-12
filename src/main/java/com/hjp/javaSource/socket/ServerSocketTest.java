325520945166681194c7d67e2c494f1085a62d1d
Java兔 <794926872@qq.com>
2020-08-12 16:01:45 +0800

    Socket测试类提交。

--- src/main/java/com/hjp/javaSource/socket/ServerSocketTest.java
+++ src/main/java/com/hjp/javaSource/socket/ServerSocketTest.java
@@ -20,25 +20,18 @@ public class ServerSocketTest {
         // 初始化服务端socket并且绑定1111端口
         ServerSocket serverSocket  = new ServerSocket(1111);
         System.out.println("server socket连接已就绪，开始接收信息...");
+
         //等待客户端的连接
-        Socket socket = serverSocket.accept();
-
-        // 获取输入流
-        InputStream inputStream = socket.getInputStream();
-        InputStreamReader in = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader =new BufferedReader(in);
-
-        // 读取一行数据
-        String msg;
         while (true){
-            msg = bufferedReader.readLine();
-            if (msg != null){
-                System.out.println(msg);
-            }
+            Socket socket = serverSocket.accept();
+
+            // 获取输入流
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader in = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader =new BufferedReader(in);
+
+            // 读取一行数据
+            System.out.println(bufferedReader.readLine());
         }
-
-
-
-
     }
 }
